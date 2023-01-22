1914fc7f248ecd81a8acc3df5bf08f4bf821710c
Lucas Medeiros <medeiros.lucas1993@gmail.com>
2023-01-22 12:10:46 -0300

    REFACTOR: removido render do navegador

--- src/puppeteer/createBrowser.js
+++ src/puppeteer/createBrowser.js
@@ -6,7 +6,7 @@ export async function CreateBrowser() {
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36";
 
     const browser = await launch({
-      headless: false,
+      headless: true,
       slowMo: 100,
     });
 
